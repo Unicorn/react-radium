02b9b6c573a07326ecd26054e3a25e17192c7c07
Zack Perdue <zack@ziggidycreative.com>
2022-08-15 01:11:45 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './stories/*'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
